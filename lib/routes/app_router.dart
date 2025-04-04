950eb2d1fd89139a714d2851ac6c23d2f38abb51
Jcamiloag <juan.arevalo01@uceva.edu.co>
2025-04-04 11:38:17 -0500

    Crud de establecimientos funcionando

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,19 +1,29 @@
 import 'package:go_router/go_router.dart';
-import 'package:hola_mundo/views/ciclo_vida/ciclo_vida_screen.dart';
-import 'package:hola_mundo/views/future/future_view.dart';
+import 'package:flutter/material.dart';
+import 'package:hola_mundo/views/establecimientos/establecimiento_list_view.dart';
+
+// Vistas generales
 import 'package:hola_mundo/views/home_view.dart';
+import 'package:hola_mundo/views/settings_view.dart';
+import 'package:hola_mundo/views/profile_view.dart';
 import 'package:hola_mundo/views/paso_parametros/detalle_screen.dart';
 import 'package:hola_mundo/views/paso_parametros/paso_parametros_screen.dart';
-import 'package:hola_mundo/views/profile_view.dart';
-import 'package:hola_mundo/views/settings_view.dart';
+import 'package:hola_mundo/views/ciclo_vida/ciclo_vida_screen.dart';
 import 'package:hola_mundo/views/future/future_view.dart';
 import 'package:hola_mundo/views/timer/timer_view.dart';
 import 'package:hola_mundo/views/isolate/isolate_view.dart';
+
+// Vistas de perros
 import 'package:hola_mundo/views/dog/dog_list_view.dart';
 import 'package:hola_mundo/views/dog/dog_detail_view.dart' as dogDetail;
 
+// Vistas de establecimientos
+import 'package:hola_mundo/views/establecimientos/establecimiento_edit_view.dart';
+import 'package:hola_mundo/views/establecimientos/establecimiento_create_views.dart';
+
 final GoRouter appRouter = GoRouter(
   routes: [
+    // Home y generales
     GoRoute(
       path: '/',
       builder: (context, state) => const HomeView(),
@@ -26,6 +36,8 @@ final GoRouter appRouter = GoRouter(
       path: '/profile',
       builder: (context, state) => const ProfileView(),
     ),
+
+    // Paso de parámetros
     GoRoute(
       path: '/paso_parametros',
       builder: (context, state) => const PasoParametrosScreen(),
@@ -38,6 +50,8 @@ final GoRouter appRouter = GoRouter(
         return DetalleScreen(parametro: parametro, metodoNavegacion: metodo);
       },
     ),
+
+    // Otros ejemplos
     GoRoute(
       path: '/ciclo_vida',
       builder: (context, state) => const CicloVidaScreen(),
@@ -57,6 +71,8 @@ final GoRouter appRouter = GoRouter(
       name: 'isolate',
       builder: (context, state) => const IsolateView(),
     ),
+
+    // Perros
     GoRoute(
       name: 'dogListView',
       path: '/dogs',
@@ -70,5 +86,29 @@ final GoRouter appRouter = GoRouter(
         return dogDetail.DogDetailView(breed: breed);
       },
     ),
+
+    // Establecimientos
+    GoRoute(
+      path: '/establecimientos',
+      builder: (context, state) => const EstablecimientosListView(),
+    ),
+    GoRoute(
+      path: '/establecimientos',
+      name: 'establecimientos',
+      builder: (context, state) => const EstablecimientosListView(),
+    ),
+    //!Ruta para editar de un establecimiento
+    GoRoute(
+      path: '/establecimientos/edit/:id',
+      builder: (context, state) {
+        //*se captura el id del establecimiento
+        final id = int.parse(state.pathParameters['id']!);
+        return EstablecimientoEditView(id: id);
+      },
+    ),
+    GoRoute(
+      path: '/establecimientos/create',
+      builder: (context, state) => const EstablecimientoCreateView(),
+),
   ],
 );
--- lib/views/base_view.dart
+++ lib/views/base_view.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
 import 'custom_drawer.dart'; // Importa el Drawer personalizado
+import 'package:go_router/go_router.dart'; // Para navegación
 
 class BaseView extends StatelessWidget {
   final String title;
-  final Widget body; // Cuerpo de la vista
-  final int initialIndex; // Índice inicial para el BottomNavigationBar
-  final int length; // Longitud de la lista de rutas
+  final Widget body;
+  final int initialIndex;
+  final int length;
+  final Widget? floatingActionButton;
 
-  const BaseView({super.key, required this.title, required this.body, required this.initialIndex, required this.length});
+  const BaseView({
+    super.key,
+    required this.title,
+    required this.body,
+    required this.initialIndex,
+    required this.length,
+    this.floatingActionButton,
+  });
+
+  void _onTap(int index, BuildContext context) {
+    if (index == initialIndex) return;
+
+    switch (index) {
+      case 0:
+        context.go('/establecimientos');
+        break;
+      case 1:
+        context.go('/dogs');
+        break;
+      case 2:
+        context.go('/profile');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title), backgroundColor: Colors.red,),
-      drawer: const CustomDrawer(), // Drawer persistente para todas las vistas
+      appBar: AppBar(title: Text(title), backgroundColor: Colors.red),
+      drawer: const CustomDrawer(),
       body: body,
+      floatingActionButton: floatingActionButton,
     );
   }
 }
